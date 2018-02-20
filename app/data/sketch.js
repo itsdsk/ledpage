99c249cfd6d2141312ccadaf99cfc7434d4e5d69
dsk <dsuttonklein@mail.com>
2018-02-20 10:31:07 +0000

    add p5.js sketch

---
+++ app/data/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  // put setup code here
+  createCanvas(1280, 720);
+  colorMode(HSB, 255);
+
+}
+var c = 0;
+
+function draw() {
+  // put drawing code here
+  //if (c >= 255)  c=0;  else  c++;
+  //background(c, 255, 255);
+  ellipse(50, 50, 80, 80);
+  if (c >= 255)  c=0;  else  c++;
+  background(c, 255, 255);
+
+
+}
