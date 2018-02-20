455be07fe402f2acb915355d3161ddd4a9d75c20
dsk <dsuttonklein@mail.com>
2018-02-20 13:36:59 +0000

    use recommended hyperion arduino sketch from https://hyperion-project.org/threads/diy-amblight-project-guide-hyperion-ws2801-ws2812b-apa102.8/

--- app/arduino_display/arduino_display.ino
+++ app/arduino_display/arduino_display.ino
@@ -4,60 +4,80 @@
 #include "FastLED.h"
 #define DATA_PIN 5
 #define NUM_LEDS 32
+#define COLOR_ORDER BRG
 CRGB leds[NUM_LEDS];
 
 // baudrate (higher is faster refresh rate)
 #define serialRate 460800
 
 // data header
-const uint8_t header[4] = { 0xDE, 0xAD, 0xBE, 0xEF };
+uint8_t prefix[] = {'A', 'd', 'a'}, hi, lo, chk, i;
+//const uint8_t header[4] = { 0xDE, 0xAD, 0xBE, 0xEF };
 
 void setup()
 {
-  // initialise serial USB communication
-  Serial.begin(serialRate);
-
   // initialise LEDs
   FastLED.addLeds<WS2812B, DATA_PIN, BRG>(leds, NUM_LEDS);
 
   // initialise randomness
   randomSeed(analogRead(0));
+
+  // initial RGB flash
+  LEDS.showColor(CRGB(255, 0, 0));
+  delay(500);
+  LEDS.showColor(CRGB(0, 255, 0));
+  delay(500);
+  LEDS.showColor(CRGB(0, 0, 255));
+  delay(500);
+  LEDS.showColor(CRGB(0, 0, 0));
+
+  // initialise serial USB communication
+  Serial.begin(serialRate);
+  Serial.print("Ada\n"); // Send "Magic Word" string to host
 }
 
 
-void loop()
-{
-  // reference:
-  // https://github.com/FastLED/FastLED/wiki/Interrupt-problems
-
-  while(true) {
-    while(Serial.available() == false){} // wait for serial data
-    uint8_t b = Serial.read(); // read first byte
-    bool looksLikeHeader = false;
-    if(b == header[0]){ // check if first byte is header
-      looksLikeHeader = true;
-      for(int i = 1; looksLikeHeader && (i < sizeof(header)); i++) {
-        while(Serial.available() == 0){} // wait for new serial data
-        b = Serial.read();
-        if(b != header[i]) {
-          // whoops, not a match, this no longer looks like a header.
-          looksLikeHeader = false;
-        }
-      }
-    }
-    if(looksLikeHeader){
-      int bytesRead = 0;
-      // read bytes to buffer
-      while(bytesRead < (NUM_LEDS*3)){
-        bytesRead += Serial.readBytes((char*)(((uint8_t*)leds) +
-                      bytesRead), (NUM_LEDS*3)-bytesRead);
-      }
-      break; // break out of while loop
-    }
+void loop() {
+  // wait for first byte of Magic Word
+  for(i = 0; i < sizeof prefix; ++i) {
+    waitLoop: while (!Serial.available()) ;;
+    // Check next byte in Magic Word
+    if(prefix[i] == Serial.read()) continue;
+    // otherwise, start over
+    i = 0;
+    goto waitLoop;
   }
-  // output to LEDs
-  FastLED.show();
-
-  // finally, flush out any data in the serial buffer, as it may have been interrupted oddly by writing out led data:
-  while(Serial.available() > 0) { Serial.read(); }
+ 
+  // Hi, Lo, Checksum
+ 
+  while (!Serial.available()) ;;
+  hi=Serial.read();
+  while (!Serial.available()) ;;
+  lo=Serial.read();
+  while (!Serial.available()) ;;
+  chk=Serial.read();
+ 
+  // if checksum does not match go back to wait
+  if (chk != (hi ^ lo ^ 0x55))
+  {
+    i=0;
+    goto waitLoop;
+  }
+ 
+  memset(leds, 0, NUM_LEDS * sizeof(struct CRGB));
+  // read the transmission data and set LED values
+  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+    byte r, g, b;    
+    while(!Serial.available());
+    r = Serial.read();
+    while(!Serial.available());
+    g = Serial.read();
+    while(!Serial.available());
+    b = Serial.read();
+    leds[i].r = r;
+    leds[i].g = g;
+    leds[i].b = b;
+  }
+  // shows new values
+ FastLED.show();
 }
