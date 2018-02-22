b728fee1ff792d35545437da0932e528822b9d86
dsk <dsuttonklein@mail.com>
2018-02-22 19:26:17 +0000

    led layout: add virtual path prefix to static files in express (try fix public files not linking because the URL is a subdirectory)

--- app/d3/d3server.js
+++ app/d3/d3server.js
@@ -9,7 +9,7 @@ var app = express();
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static(__dirname + '/public'));
+app.use("/setup", express.static(__dirname + '/public'));
 
 
 // http://expressjs.com/en/starter/basic-routing.html
