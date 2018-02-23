4ab6dbe57487489c5459fab0138c667c1d2bda38
dsk <dsuttonklein@mail.com>
2018-02-23 00:20:47 +0000

    start.sh: reorder the execution of subprocesses so dependent ones start last, and compile arduino sketch inbetween

--- app/start.sh
+++ app/start.sh
@@ -3,8 +3,22 @@
 # kill all subshells and processes on exit
 trap "kill 0" SIGINT
 
+# start mongodb
+( mongod ) &
 
-# check and update arduino board
+# start electron
+umount /dev/shm && mount -t tmpfs shm /dev/shm
+rm /tmp/.X0-lock &>/dev/null || true
+( startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging ) &
+
+# start webserver/led layout
+( cd /usr/src/app/d3 && /usr/local/bin/node /usr/src/app/d3/d3server.js ) &
+
+# start hyperion
+( /usr/bin/hyperiond /usr/src/app/hyperion.config.json ) &
+
+# compile and update arduino
+cd /usr/src/app/arduino_display && make
 diff /usr/src/app/arduino_display/arduino_display.ino /data/arduino_display.ino || PROGRAMMER=1
 if [ "${PROGRAMMER:-}" == "1" ]; then
   echo $PROGRAMMER
@@ -14,33 +28,11 @@ if [ "${PROGRAMMER:-}" == "1" ]; then
   popd
 fi
 
-# start mongodb
-( mongod ) &
-
 # start webserver/cms
 ( cd /usr/src/app/cms && /usr/local/bin/node /usr/src/app/cms/keystone.js ) &
-
-# start webserver/led layout
-( cd /usr/src/app/d3 && /usr/local/bin/node /usr/src/app/d3/d3server.js ) &
 
 # start nginx
 ( nginx -g "daemon off;" ) &
 
-# electron
-
-# By default docker gives us 64MB of shared memory size but to display heavy
-# pages we need more.
-umount /dev/shm && mount -t tmpfs shm /dev/shm
-
-# using local electron module instead of the global electron lets you
-# easily control specific version dependency between your app and electron itself.
-# the syntax below starts an X istance with ONLY our electronJS fired up,
-# it saves you a LOT of resources avoiding full-desktops envs
-
-rm /tmp/.X0-lock &>/dev/null || true
-( startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging ) &
-
-# start hyperion
-( /usr/bin/hyperiond /usr/src/app/hyperion.config.json ) &
-
+# wait for subprocesses to finish
 wait
