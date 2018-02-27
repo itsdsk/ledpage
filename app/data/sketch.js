7a163f7513caaddfe349f002039c4e68ac93b508
dsk <dsuttonklein@mail.com>
2018-02-27 14:14:17 +0000

    led setup: add d3 dependency (and clean sketch.js code)

--- app/data/sketch.js
+++ app/data/sketch.js
@@ -1,5 +1,4 @@
 function setup() {
-  // put setup code here
   createCanvas(1280, 720);
   frameRate(30);
   colorMode(HSB, 255);
@@ -8,12 +7,7 @@ function setup() {
 var c = 0;
 
 function draw() {
-  // put drawing code here
-  //if (c >= 255)  c=0;  else  c++;
-  //background(c, 255, 255);
   ellipse(50, 50, 80, 80);
   if (c >= 255)  c=0;  else  c++;
   background(c, 255, 128);
-
-
 }
