2db91f4ce930bd7e8458c259b1372a617a66204a
dsk <dsuttonklein@mail.com>
2018-02-28 11:52:32 +0000

    start.sh: instantiate cms and nginx later (as they depend on mongodb and nodejs processes which run first)

--- app/start.sh
+++ app/start.sh
@@ -6,22 +6,20 @@ trap "kill 0" SIGINT
 # start mongodb
 ( mongod ) &
 
-sleep 5
+sleep 15
 
 # start electron
 umount /dev/shm && mount -t tmpfs shm /dev/shm
 rm /tmp/.X0-lock &>/dev/null || true
 ( startx /usr/src/app/node_modules/electron/dist/electron /usr/src/app --enable-logging ) &
 
+# start hyperion
+( /usr/bin/hyperiond /usr/src/app/hyperion_config/hyperion.config.json ) &
+
 sleep 5
 
 # start webserver/led layout
 ( cd /usr/src/app/d3 && /usr/local/bin/node /usr/src/app/d3/d3server.js ) &
-
-sleep 5
-
-# start hyperion
-( /usr/bin/hyperiond /usr/src/app/hyperion_config/hyperion.config.json ) &
 
 sleep 5
 
@@ -31,7 +29,7 @@ sleep 5
 sleep 5
 
 # start nginx
-( nginx -g "daemon off;" ) &
+( /usr/sbin/nginx -g "daemon off;" ) &
 
 # wait
 sleep 30
