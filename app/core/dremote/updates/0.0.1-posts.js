2be1a0b8dc299e020cd7b5ae4b3e96d7133efd6b
dsk <dsuttonklein@mail.com>
2018-03-05 20:40:03 +0000

    dremote: add keystone update to init new Post, and add sketch to persistant docker storage (/data/) which the first Post links to

---
+++ app/core/dremote/updates/0.0.1-posts.js
@@ -0,0 +1,8 @@
+exports.create = {
+    Post: [{
+        'title': 'Post1',
+        'state': 'published',
+        //'author': 'user@keystonejs.com',
+        'localPath': '/data/sketches/sketch1/',
+    }, ],
+};
