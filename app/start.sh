e7046cc949a028355b02829dd743c799f8a9d405
dsk <dsuttonklein@mail.com>
2018-03-05 18:22:57 +0000

    disable IPFS and dsharing component

--- app/start.sh
+++ app/start.sh
@@ -7,7 +7,7 @@ pm2 start /usr/src/app/core/dsetup/d3server.js --cwd "/usr/src/app/core/dsetup/"
 pm2 start /usr/src/app/core/dremote/keystone.js --cwd "/usr/src/app/core/dremote/" --name "dremote"
 
 # start sharing component
-pm2 start /usr/src/app/core/dsharing/app.js --cwd "/usr/src/app/core/dsharing/" --name "dsharing"
+#pm2 start /usr/src/app/core/dsharing/app.js --cwd "/usr/src/app/core/dsharing/" --name "dsharing"
 
 # start electron
 umount /dev/shm && mount -t tmpfs shm /dev/shm
