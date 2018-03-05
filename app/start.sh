69762114dc8020fda968354553a8f543ea2b7885
dsk <dsuttonklein@mail.com>
2018-03-05 12:53:44 +0000

    start.sh: set exec directory in which pm2 processes are launched

--- app/start.sh
+++ app/start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # start webserver/led layout
-pm2 start /usr/src/app/core/dsetup/d3server.js
+pm2 start /usr/src/app/core/dsetup/d3server.js --cwd "/usr/src/app/core/dsetup/"
 
 # start webserver/cms
-pm2 start /usr/src/app/core/dremote/keystone.js
+pm2 start /usr/src/app/core/dremote/keystone.js --cwd "/usr/src/app/core/dremote/"
 
 # start sharing component
-pm2 start /usr/src/app/core/dsharing/app.js
+pm2 start /usr/src/app/core/dsharing/app.js --cwd "/usr/src/app/core/dsharing/"
 
 # start electron
 umount /dev/shm && mount -t tmpfs shm /dev/shm
