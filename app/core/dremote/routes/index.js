0c4a99d09d4f9fd69b77a313916a19cd72a58fab
dsk <dsuttonklein@mail.com>
2018-03-05 17:30:42 +0000

    dremote: added basic files for upload sketch function (following step 4: http://keystonejs.netlify.com/getting-started/setting-up/part-4/)

--- app/core/dremote/routes/index.js
+++ app/core/dremote/routes/index.js
@@ -3,8 +3,11 @@ var importRoutes = keystone.importer(__dirname);
 
 var routes = {
   views: importRoutes('./views'),
+  api: importRoutes('./api'),
 };
 
 exports = module.exports = function (app) {
   app.get('/', routes.views.index)
+  app.get('/add-sketch', routes.views.addSketch)
+  app.post('/api/sketch', api.sketch.post)
 };
