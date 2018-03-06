dd6dd9dd347b99ead285f62d75824e218913d27b
dsk <dsuttonklein@mail.com>
2018-03-06 16:07:22 +0000

    dremote: correct sketch paths (prefix 'file:///')

--- app/core/dremote/updates/0.0.1-posts.js
+++ app/core/dremote/updates/0.0.1-posts.js
@@ -3,13 +3,13 @@ exports.create = {
             'title': 'RGB Fade',
             'state': 'published',
             //'author': 'user@keystonejs.com',
-            'localPath': '/data/sketches/sketch1/index.html',
+            'localPath': 'file:////data/sketches/sketch1/index.html',
         },
         {
             'title': 'Intensity Pulse',
             'state': 'published',
             //'author': 'user@keystonejs.com',
-            'localPath': '/data/sketches/sketch2/index.html',
+            'localPath': 'file:////data/sketches/sketch2/index.html',
         }
     ],
 };
