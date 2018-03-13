9b58dbdebe5b67c5fd90d7cf730273e804213beb
dsk <dsuttonklein@mail.com>
2018-03-13 17:51:21 +0000

    dsharing: update sketch with ipfsHash and channel

--- app/core/dremote/updates/0.0.1-channels.js
+++
@@ -1,5 +0,0 @@
-exports.create = {
-    SketchChannel: [{
-        'name': 'sketches'
-    }],
-};
--- app/core/dremote/updates/0.0.1-sketches.js
+++ app/core/dremote/updates/0.0.1-sketches.js
@@ -1,4 +1,7 @@
 exports.create = {
+    SketchChannel: [{
+        'name': 'sketches'
+    }],
     Sketch: [{
             'title': 'RGB Fade',
             'state': 'published',
@@ -10,6 +13,8 @@ exports.create = {
             'state': 'published',
             //'author': 'user@keystonejs.com',
             'localDir': 'sketch2',
+            'ipfsHash': 'QmaqNgz2fBVorb2rb4WF55fBq6Vymaenj5HT7oeMVJtJyS',
+            'channels': 'sketches'
         },
         {
             'title': 'Decentralised',
