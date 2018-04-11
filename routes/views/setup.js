de87166d9e68afd487c65fe946ffac67999cbe9e
dsk <dsuttonklein@mail.com>
2018-04-11 18:27:30 +0100

    add LED chipsets to config options (from FastLED reference: https://github.com/FastLED/FastLED/wiki/Chipset-reference)

--- routes/views/setup.js
+++ routes/views/setup.js
@@ -21,15 +21,91 @@ exports = module.exports = function (req, res) {
         value: "13",
         label: "13"
     }];
-    locals.ledChips = [{
+    locals.ledChips = [
+        {
+            value: "APA102",
+            label: "APA102 (also Dotstar)",
+            pins: "4pin"
+        },
+        {
             value: "WS2812B",
             label: "WS2812B (also Neopixel)",
             pins: "3pin"
         },
         {
-            value: "APA102",
-            label: "APA102 (also Dotstar)",
+            value: "WS2813",
+            label: "WS2813",
+            pins: "3pin"
+        },
+        {
+            value: "WS2812",
+            label: "WS2812",
+            pins: "3pin"
+        },
+        {
+            value: "WS2811",
+            label: "WS2811",
+            pins: "3pin"
+        },
+        {
+            value: "WS2801",
+            label: "WS2801",
             pins: "4pin"
+        },
+        {
+            value: "TM1812",
+            label: "TM1812",
+            pins: "3pin"
+        },
+        {
+            value: "TM1809",
+            label: "TM1809",
+            pins: "3pin"
+        },
+        {
+            value: "TM1804",
+            label: "TM1804",
+            pins: "3pin"
+        },
+        {
+            value: "TM1803",
+            label: "TM1803",
+            pins: "3pin"
+        },
+        {
+            value: "UCS2903",
+            label: "UCS2903",
+            pins: "3pin"
+        },
+        {
+            value: "UCS1903",
+            label: "UCS1903",
+            pins: "3pin"
+        },
+        {
+            value: "LPD8806",
+            label: "LPD8806",
+            pins: "4pin"
+        },
+        {
+            value: "SM16716",
+            label: "SM16716",
+            pins: "4pin"
+        },
+        {
+            value: "P9813",
+            label: "P9813",
+            pins: "4pin"
+        },
+        {
+            value: "GW6205",
+            label: "GW6205",
+            pins: "3pin"
+        },
+        {
+            value: "APA104",
+            label: "APA104",
+            pins: "3pin"
         }
     ];
     locals.ledOrders = [{
