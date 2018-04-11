66d14bd5c699e652cfa27329d6e0e856fd02fcad
dsk <dsuttonklein@mail.com>
2018-04-11 17:31:48 +0100

    start.sh: remove commented out commands

--- start.sh
+++ start.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # start webserver/cms
-# pm2 start /usr/src/app/core/dremote/keystone.js --cwd "/usr/src/app/core/dremote/" --name "dremote"
 pm2 start /usr/src/app/app.js --cwd "/usr/src/app/" --name "remoteapp"
 
 # start electron
 umount /dev/shm && mount -t tmpfs shm /dev/shm
 rm /tmp/.X0-lock &>/dev/null || true
-#startx /usr/src/app/core/dplayer/node_modules/electron/dist/electron /usr/src/app/core/dplayer --enable-logging
 startx /usr/src/app/libs/player/node_modules/electron/dist/electron /usr/src/app/libs/player --enable-logging
