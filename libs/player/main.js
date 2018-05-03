42585702ff9fe104dcbc8ca378e3beeccb87145a
dsk <dsuttonklein@mail.com>
2018-05-03 17:47:42 +0100

    add favicon to web ui, remove hardset output config files, remove commented out code in dplayer and set electron background color

--- libs/player/main.js
+++ libs/player/main.js
@@ -12,42 +12,7 @@ const {
   BrowserWindow
 } = electron;
 
-// simple parameters initialization
-// const electronConfig = {
-// URL_LAUNCHER_TOUCH: process.env.URL_LAUNCHER_TOUCH === '1' ? 1 : 0,
-// URL_LAUNCHER_TOUCH_SIMULATE: process.env.URL_LAUNCHER_TOUCH_SIMULATE === '1' ? 1 : 0,
-// URL_LAUNCHER_FRAME: process.env.URL_LAUNCHER_FRAME === '1' ? 1 : 0,
-// URL_LAUNCHER_KIOSK: process.env.URL_LAUNCHER_KIOSK === '1' ? 1 : 0,
-// URL_LAUNCHER_NODE: process.env.URL_LAUNCHER_NODE === '1' ? 1 : 0,
-// URL_LAUNCHER_WIDTH: parseInt(process.env.URL_LAUNCHER_WIDTH || 1920, 10),
-// URL_LAUNCHER_HEIGHT: parseInt(process.env.URL_LAUNCHER_HEIGHT || 1080, 10),
-// URL_LAUNCHER_TITLE: process.env.URL_LAUNCHER_TITLE || 'RESIN.IO',
-// URL_LAUNCHER_CONSOLE: process.env.URL_LAUNCHER_CONSOLE === '1' ? 1 : 0,
-// URL_LAUNCHER_URL: process.env.URL_LAUNCHER_URL || `file:///${path.join(__dirname, 'data', 'index.html')}`,
-// URL_LAUNCHER_ZOOM: parseFloat(process.env.URL_LAUNCHER_ZOOM || 1.0),
-// URL_LAUNCHER_OVERLAY_SCROLLBARS: process.env.URL_LAUNCHER_CONSOLE === '1' ? 1 : 0,
-// };
 app.disableHardwareAcceleration();
-// enable touch events if your device supports them
-// if (electronConfig.URL_LAUNCHER_TOUCH) {
-//   app.commandLine.appendSwitch('--touch-devices');
-// }
-// // simulate touch events - might be useful for touchscreen with partial driver support
-// if (electronConfig.URL_LAUNCHER_TOUCH_SIMULATE) {
-//   app.commandLine.appendSwitch('--simulate-touch-screen-with-mouse');
-// }
-
-// if (process.env.NODE_ENV === 'development') {
-//   console.log('Running in development mode');
-//   Object.assign(electronConfig, {
-//     URL_LAUNCHER_HEIGHT: 600,
-//     URL_LAUNCHER_WIDTH: 800,
-//     URL_LAUNCHER_KIOSK: 0,
-//     URL_LAUNCHER_CONSOLE: 1,
-//     URL_LAUNCHER_FRAME: 1,
-//   });
-// }
-
 /*
  we initialize our application display as a callback of the electronJS "ready" event
  */
@@ -58,12 +23,11 @@ app.on('ready', () => {
     width: 720,
     height: 720,
     frame: false, // frameless window without chrome graphical interfaces (borders, toolbars etc)
-    // title: electronConfig.URL_LAUNCHER_TITLE,
     kiosk: true, // chromium kiosk mode (fullscreen without icons or taskbar)
+    backgroundColor: '#09f911', // set backgrounnd
     webPreferences: {
       sandbox: false,
       nodeIntegration: false,
-      // zoomFactor: electronConfig.URL_LAUNCHER_ZOOM,
       overlayScrollbars: false,
     },
   });
@@ -74,11 +38,6 @@ app.on('ready', () => {
     }, 300);
   });
 
-  // if the env-var is set to true,
-  // a portion of the screen will be dedicated to the chrome-dev-tools
-  // if (electronConfig.URL_LAUNCHER_CONSOLE) {
-  //   window.webContents.openDevTools();
-  // }
   process.on('uncaughtException', function (err) {
     console.log(err);
   });
