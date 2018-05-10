cd10ed6148aba374eb7d3a70239382a9fd0b6a78
dsk <dsuttonklein@mail.com>
2018-05-10 17:14:08 +0100

    remote: fix error where navbar tabs for media (preview+details) showed on homepage

--- routes/views/sketch.js
+++ routes/views/sketch.js
@@ -7,7 +7,7 @@ exports = module.exports = function (req, res) {
 	var view = new keystone.View(req, res);
 	var locals = res.locals;
 	// Set locals
-	locals.section = 'browse';
+	locals.section = 'media';
 	locals.filters = {
 		id: req.params.id,
 	};
