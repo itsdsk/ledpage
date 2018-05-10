dceab5727b02e0f74cfe16ab548cdfb218acbeeb
dsk <dsuttonklein@mail.com>
2018-05-10 10:59:27 +0100

    remote: set server port to variable because 'non root user cant open a listening socket on ports below 1024' (for testing)...

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ var keystone = require('keystone');
 // See http://keystonejs.com/guide/config for available options
 // and documentation.
 
+var port = 8081;
+
 keystone.init({
   'cookie secret': '05f8261c27d0ac4751627b469d7e2d9b84246bbbc4b68edc3c5b530af1010560ac63182ff59adce22e5ff44ab6e66075bd460e89803a818c183c9f4f2049e260',
   'name': 'Disks',
-  'port': 80,
+  'port': port,
   'sass': 'public',
   'static': ['public', '/data/content'],
   'favicon': 'public/favicon.ico',
@@ -58,5 +60,6 @@ keystone.set('routes', require('./routes'));
 
 keystone.start(function () {
   // initialise database
-  require('http').get('http://0.0.0.0:80/api/media/list/init');
+  var initurl = 'http://0.0.0.0:'+port+'/api/media/list/init';
+  require('http').get(initurl);
 });
