0111d888a7ac0660a730699227f95d52a0fd5c37
dsk <dsuttonklein@mail.com>
2018-12-20 18:42:45 +0000

    get content by reading /media directory, send thumbnail jpgs to client

--- app.js
+++ app.js
@@ -3,31 +3,37 @@ const app = express();
 const expressWs = require('express-ws')(app);
 const Handlebars = require('handlebars');
 const fs = require('fs');
+const path = require('path');
 
+// get items
+var media = new Array();
+fs.readdirSync('./media').filter(function (file) {
+  if (fs.statSync('./media/' + file).isDirectory()) {
+    var med = require('./media/' + file + '/demo.json');
+    media.push(med);
+  }
+});
+
+// item template
 var template;
 fs.readFile(__dirname + "/template.handlebars", function (err, data) {
   if (err) throw err;
   template = Handlebars.compile(data.toString());
 });
 
-var data = [{
-  "name": "t1",
-  "desc": "d1"
-}, {
-  "name": "t11",
-  "desc": "d11"
-}, {
-  "name": "t111",
-  "desc": "d111"
-}];
-
 app.use('/', express.static('./'));
 
 app.ws('/', function (ws, req) {
   ws.on('message', function (msg) {
-    data.forEach(element => {
-      var result = template(element);
-      ws.send(result);
+    media.forEach(element => {
+      fs.readFile('./media/item1/thumb.jpg', function (err, buf) {
+        if (err) throw err;
+        var img = "data:image/jpeg;base64," + buf.toString('base64');
+        element.img_src = img;
+        var result = template(element);
+        ws.send(result);
+      });
+      console.log(element);
     });
     console.log(msg);
   });
