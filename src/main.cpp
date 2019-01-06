b78c6aac0884d01b9fe85d58dbdc6de4b83d97a8
dsk <dsuttonklein@mail.com>
2019-01-06 12:41:18 +0000

    src: add command line options in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,12 @@
 #include <vector>
 #include <unistd.h>
 
+#include <device/DeviceManager.h>
 #include <grabber/Image.h>
 #include <grabber/ColorRgba.h>
 #include <grabber/Grabber.h>
 #include <thirdparty/json/single_include/nlohmann/json.hpp>
-#include <device/DeviceManager.h>
+#include <thirdparty/cxxopts/include/cxxopts.hpp>
 
 using namespace std;
 using json = nlohmann::json;
@@ -17,8 +18,15 @@ int jsonStringToInt(json &value)
     return std::stoi(value.get<std::string>());
 }
 
-int main()
+void saveScreenshot(Image<ColorRgba> &_image);
+
+int main(int argc, char *argv[])
 {
+    // command line options
+    cxxopts::Options options("Disk", "Display Web Media on LEDs from Raspberry Pi");
+    options.add_options()("s,screenshot", "Save screenshot as .PPM")("d,display", "Display screenshot on device");
+    auto result = options.parse(argc, argv);
+
     // read config file
     std::ifstream confFile("../www/engine/config.json");
     json config;
@@ -38,7 +46,8 @@ int main()
     FrameGrabber *grabber = new FrameGrabber(_w, _h);
     Image<ColorRgba> _image(_w, _h);
 
-    if (true) // runtime loop
+    // display screen on device
+    if (result.count("display") > 0) // runtime loop
     {
         for (int i = 0; i < 1; i++)
         {
@@ -49,27 +58,34 @@ int main()
             }
         }
     }
-    else // save screenshot
+    // save screenshot
+    if (result.count("screenshot") > 0)
     {
         grabber->grabFrame(_image);
-        // write ppm file
-        ofstream myfile;
-        myfile.open("screenshot.ppm");
-        myfile << "P6"
-               << "\n"
-               << _w << " " << _h << "\n"
-               << 255 << "\n";
-        for (uint_least32_t y = 0; y < _image.height(); y++)
-        {
-            for (uint_least32_t x = 0; x < _image.width(); x++)
-            {
-                myfile << _image(x, y).red;
-                myfile << _image(x, y).green;
-                myfile << _image(x, y).blue;
-            }
-        }
-        myfile.close();
     }
+
     delete grabber;
     return 0;
+}
+
+void saveScreenshot(Image<ColorRgba> &_image)
+{
+    // write ppm file
+    ofstream myfile;
+    myfile.open("screenshot.ppm");
+    myfile << "P6"
+           << "\n"
+           << _image.width() << " " << _image.height() << "\n"
+           << 255 << "\n";
+    for (uint_least32_t y = 0; y < _image.height(); y++)
+    {
+        for (uint_least32_t x = 0; x < _image.width(); x++)
+        {
+            myfile << _image(x, y).red;
+            myfile << _image(x, y).green;
+            myfile << _image(x, y).blue;
+        }
+    }
+    cout << "Saving screenshot" << endl;
+    myfile.close();
 }
