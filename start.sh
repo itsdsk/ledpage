652fddc781b35c04917566041383507902ee7bb0
dsk <dsuttonklein@mail.com>
2019-02-10 11:14:28 +0000

    Update start.sh script and .gitignore

--- start.sh
+++ start.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# enable mDNS <hostname>.local (connect to the host's system bus from the application container)
-export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-
-# start webserver/cms
-pm2 start /usr/src/app/app.js --cwd "/usr/src/app/" --name "remoteapp"
-
-# start renderer
-umount /dev/shm && mount -t tmpfs shm /dev/shm
-rm /tmp/.X0-lock &>/dev/null || true
-startx /usr/src/app/libs/player/node_modules/electron/dist/electron /usr/src/app/libs/player --enable-logging
+# start backend
+cd /home/pi/disk/backend && ./main -d &
+# start renderer 
+startx /home/pi/disk/renderer/node_modules/electron/dist/electron /home/pi/disk/renderer/main.js &
+# start ui
+cd ../ui && node app.js &
