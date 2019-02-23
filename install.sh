fabcd7eb06d9cf51a9ed7863eb26263f05201b08
dsk <dsuttonklein@mail.com>
2019-02-23 20:50:47 +0000

    install.sh: add systemd unit files for ui, renderer and backend

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 # for raspbian 2018-11-13-raspbian-stretch-lite.zip image
 
 # dependencies
-sudo apt update && sudo apt install xserver-xorg-core \
+sudo apt update
+sudo apt install xserver-xorg-core \
   xserver-xorg-input-all \
   xserver-xorg-video-fbturbo \
   xorg \
@@ -15,10 +16,67 @@ sudo apt update && sudo apt install xserver-xorg-core \
   libboost-all-dev
 
 # compile backend
-./backend/compile.sh
+cd ./backend
+sudo chmod +x ./compile.sh
+./compile.sh
+cd ../
+# add backend to service manager
+sudo > /etc/systemd/system/disk-backend-daemon.service
+sudo chmod 664 /etc/systemd/system/disk-backend-daemon.service
+sudo cat <<EOT >> /etc/systemd/system/disk-backend-daemon.service
+[Unit]
+Description=Disk Backend
+After=disk-renderer-daemon.service
+
+[Service]
+ExecStart=/home/pi/disk/backend/main -d
+
+[Install]
+WantedBy=multi-user.target
+EOT
+systemctl enable disk-backend-daemon
+
+# get renderer dependencies
+cd ./renderer/
+npm install
+cd ../
+# add renderer to service manager
+sudo > /etc/systemd/system/disk-renderer-daemon.service
+sudo chmod 664 /etc/systemd/system/disk-renderer-daemon.service
+sudo cat <<EOT >> /etc/systemd/system/disk-renderer-daemon.service
+[Unit]
+Description=Disk Renderer
+After=disk-ui-daemon.service
+
+[Service]
+ExecStart=/usr/bin/startx /home/pi/disk/renderer/node_modules/electron/dist/electron /home/pi/disk/renderer/main.js
+
+[Install]
+WantedBy=multi-user.target
+EOT
+systemctl enable disk-renderer-daemon
 
 # get app dependencies
-cd ./ui/ && npm install
+cd ./ui/
+npm install
+cd ../
+# add renderer to service manager
+sudo > /etc/systemd/system/disk-ui-daemon.service
+sudo chmod 664 /etc/systemd/system/disk-ui-daemon.service
+sudo cat <<EOT >> /etc/systemd/system/disk-ui-daemon.service
+[Unit]
+Description=Disk UI
+
+[Service]
+User=root
+WorkingDirectory=/home/pi/disk/ui
+ExecStart=/usr/bin/node app.js
+
+[Install]
+WantedBy=multi-user.target
+EOT
+systemctl enable disk-ui-daemon
 
 # edit /etc/X11/Xwrapper.config to include the line:
 # allowed_users=anybody
+
