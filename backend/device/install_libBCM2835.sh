94900b8703e3750756c9cf16a193a4ebc5f706b9
Daniel <daniel@random-international.com>
2019-11-11 21:34:16 +0000

    add commands to install BCM2835 library

---
+++ backend/device/install_libBCM2835.sh
@@ -0,0 +1,9 @@
+# install SPI library
+
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
+tar zxvf bcm2835-1.60.tar.gz
+cd bcm2835-1.60/
+./configure
+make
+sudo make check
+sudo make install
