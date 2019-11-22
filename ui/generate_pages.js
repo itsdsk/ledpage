c6b406889e03a11a751d3b23d4674c727a05037f
Daniel <daniel@random-international.com>
2019-11-22 16:41:39 +0000

    ui: add script to generate modular HTML pages using templates (header, footer) and views (home, settings)

---
+++ ui/generate_pages.js
@@ -0,0 +1,45 @@
+const Handlebars = require('handlebars');
+const fs = require('fs');
+const path = require('path');
+
+
+// read partials directory
+fs.readdirSync(path.join(__dirname, `partials`)).forEach(file => {
+    // read file
+    fs.readFile(path.join(__dirname, `partials`, file), function (err, data) {
+        if (err) throw err;
+        // register partial
+        console.log(`Registered partial: ${path.basename(file, `.hbs`)}`);
+        Handlebars.registerPartial(path.basename(file, `.hbs`), data.toString());
+    });
+});
+
+// read views directory
+fs.readdir(path.join(__dirname, `views`), function (err, files) {
+    if (err) throw err;
+    files.forEach(file => {
+        // read file
+        fs.readFile(path.join(__dirname, `views`, file), function (err, data) {
+            if (err) throw err;
+            // compile page
+            var compiler = Handlebars.compile(data.toString());
+            var rendered = compiler({});
+            // save to file
+            if (file.startsWith(`home`)) {
+                fs.writeFile(path.join(__dirname, `public`, `index.html`), rendered, function (err) {
+                    if (err) throw err;
+                    console.log(`Generated view: ${path.basename(file, `.hbs`)}`);
+                });
+            } else {
+                // check folder exists
+                fs.mkdir(path.join(__dirname, `public`, path.basename(file, `.hbs`)), err => {
+                    if (err && err.code != 'EEXIST') throw 'up';
+                    fs.writeFile(path.join(__dirname, `public`, path.basename(file, `.hbs`), `index.html`), rendered, function (err) {
+                        if (err) throw err;
+                        console.log(`Generated view: ${path.basename(file, `.hbs`)}`);
+                    });
+                });
+            }
+        })
+    })
+})
