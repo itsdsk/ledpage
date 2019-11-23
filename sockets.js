2d20241c064a0ce5ee697a9052c07f4e5bd1d603
Daniel <daniel@random-international.com>
2019-11-23 16:25:06 +0000

    logging/comments

--- sockets.js
+++ sockets.js
@@ -7,8 +7,10 @@ var connectFlag = true;
 process.argv.forEach(function (val, index, array) {
     if (val == '--connect' || val == '-c') {
         connectFlag = true;
+        console.log(`Connecting to backend and renderer`);
     } else if (val == '--no-connect' || val == '-n') {
         connectFlag = false;
+        console.log(`Not connecting to backend and renderer`);
     }
 });
 
@@ -18,7 +20,7 @@ exports.DomainClient = class DomainClient {
         // 
         this.name = name;
         //
-        this.socketname = "/tmp/" + this.name + ".sock";
+        this.socketname = `/tmp/${this.name}.sock`;
         //
         this.connected = false;
         this.socket = null;
@@ -26,7 +28,7 @@ exports.DomainClient = class DomainClient {
         this.connectInterval = null;
         //
         if (connectFlag) {
-            console.log("Constructing client " + this.name + " with path " + this.socketname);
+            console.log(`Constructing client ${this.name} with path ${this.socketname}`);
             this.startConnecting();
         }
     }
@@ -37,29 +39,28 @@ exports.DomainClient = class DomainClient {
     }
 
     connectToSocket() {
-        console.log("Connecting to " + this.name);
+        console.log(`Connecting to ${this.name}`);
         this.socket = net.createConnection(this.socketname)
             .on('connect', () => {
-                console.log("Connected to " + this.name);
                 this.connected = true;
                 clearInterval(this.connectInterval);
-                console.log("... Connected to " + this.name);
+                console.log(`Connected to ${this.name}`);
             })
             .on('data', (data) => {
-                console.log("Received data from " + this.name + ": " + data.toString());
+                console.log(`Received data from ${this.name}: ${data.toString()}`);
                 this.event.emit('data', data);
             })
             .on('end', () => {
-                console.log(this.name + " ended communiction");
+                console.log(`${this.name} ended communiction`);
                 this.connected = false;
                 this.socket.end();
                 this.startConnecting();
             })
             .on('close', () => {
-                console.log(this.name + " communiction closed");
+                console.log(`${this.name} communiction closed`);
             })
             .on('error', (data) => {
-                console.log("Error communicating with " + this.name + ": " + data);
+                console.log(`Error communicating with ${this.name}: ${data}`);
                 this.connected = false;
                 this.socket.end();
                 this.startConnecting();
