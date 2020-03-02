327bee5176781e9179b2f398843f85b1c85f4095
itsdsk <dsuttonklein@mail.com>
2020-03-02 01:14:26 +0000

    add sorting options for media feed ('most viewed' and 'recently added') to work with new playlist filtering

--- public/svelte-components/src/MediaFeed.svelte
+++ public/svelte-components/src/MediaFeed.svelte
@@ -4,6 +4,7 @@
   let mediaFeedObjects = [];
   socket.on("mediafeed", function(newMediaFeedObjects) {
     mediaFeedObjects = newMediaFeedObjects;
+    updateSorting();
   });
 
   let channelObjects = [];
@@ -12,17 +13,44 @@
   });
 
   let selectedChannel = 'all media';
+  let sortModes = [
+      "Recently added",
+      "Most viewed"
+  ];
+  let selectedSortMode = 'Recently added';
+
+  function updateSorting() {
+    if (selectedSortMode === 'Most viewed') {
+      // sort playcount high to low
+      mediaFeedObjects = mediaFeedObjects.sort((a, b) => b.playcount - a.playcount);
+    } else if (selectedSortMode === 'Recently added') {
+      // sort date new to old
+      mediaFeedObjects = mediaFeedObjects.sort((a, b) => Date.parse(b.modified) - Date.parse(a.modified));
+    } else {
+      console.log(`error in update sorting`);
+    }
+  }
+
 </script>
 
-<select bind:value={selectedChannel} on:change="{() => console.log(selectedChannel)}">
+<select bind:value={selectedChannel}>
   {#each channelObjects as channelObject}
     <option value={channelObject.channel_name || 'all media'}>
       {channelObject.channel_name || 'all media'} ({channelObject.count})
     </option>
   {/each}
 </select>
+
+<select bind:value={selectedSortMode} on:change="{updateSorting}">
+  {#each sortModes as sortMode}
+    <option value={sortMode}>
+      {sortMode}
+    </option>
+  {/each}
+</select>
+
 {#each mediaFeedObjects.filter(m => selectedChannel === 'all media' || m.channels.includes(selectedChannel)) as mediaFeedObject}
-  <p>{mediaFeedObject.title} ({JSON.stringify(mediaFeedObject.channels)})</p>
+  <p>{mediaFeedObject.modified} {mediaFeedObject.playcount} {mediaFeedObject.title}</p>
 {/each}
 
 <p>
