cc56cd17237cb9c896bde2d8ace5041b5b31669e
Daniel <daniel@random-international.com>
2020-03-04 05:58:26 +0000

    client ui: add svelte element for system configuration

--- public/svelte-components/src/main.js
+++ public/svelte-components/src/main.js
@@ -1,8 +1,13 @@
 import PlaybackStatus from './PlaybackStatus.svelte';
 import MediaFeed from './MediaFeed.svelte';
+import Configuration from './Configuration.svelte';
 
 new PlaybackStatus({
 	target: document.querySelector('#playback-status')
+});
+
+new Configuration({
+	target: document.querySelector('#configuration')
 });
 
 new MediaFeed({
