9bd2e0f8cfefd5062c2a764b2a356f9a961afa41
Daniel <daniel@random-international.com>
2020-03-04 06:55:54 +0000

    client ui: add slider input component to configuration

--- public/svelte-components/src/Configuration.svelte
+++ public/svelte-components/src/Configuration.svelte
@@ -1,6 +1,34 @@
 <script>
+  import ConfigurationSlider from "./ConfigurationSlider.svelte";
+  let config;
   socket.on("configuration", function(conf) {
-    // test
-    console.log(JSON.stringify(conf, null, 2));
+    config = conf;
+    // set values from config
+    brightness.value = config.settings.brightness;
+    desaturation.value = config.settings.desaturation;
+    gamma.value = config.settings.gamma;
+    blur.value = config.settings.blur;
+    fade.value = config.settings.fade;
   });
+  let brightness = {
+    name: "brightness",
+    value: 0.0125,
+    min: 0.0,
+    max: 1.0,
+    step: 0.0005
+  };
+  let desaturation = {
+    name: "desaturation",
+    value: 0.0,
+    min: 0.0,
+    max: 1.0,
+    step: 0.01
+  };
+  let gamma = { name: "gamma", value: 2.2, min: 0.0, max: 5.0, step: 0.01 };
+  let blur = { name: "blur", value: 50, min: 1, max: 48, step: 1 };
+  let fade = { name: "fade", value: 25000, min: 0, max: 25000, step: 100 };
 </script>
+
+{#each [brightness, desaturation, gamma, blur, fade] as item}
+  <ConfigurationSlider {...item} />
+{/each}
---
+++ public/svelte-components/src/ConfigurationSlider.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let name;
+  export let value;
+  export let min;
+  export let max;
+  export let step;
+</script>
+
+<p>{name} {value}</p>
+<input type="range" bind:value {min} {max} {step} />
