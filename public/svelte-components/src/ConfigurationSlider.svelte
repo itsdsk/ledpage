e0c983edd4b0d5aaab58fc6e1bb9411d8b98bbff
Daniel <daniel@random-international.com>
2020-03-10 21:30:31 +0000

    client: add slider change handler that sends new value to server

--- public/svelte-components/src/ConfigurationSlider.svelte
+++ public/svelte-components/src/ConfigurationSlider.svelte
@@ -4,7 +4,15 @@
   export let min;
   export let max;
   export let step;
+
+  function handleChange (event) {
+    var data = {
+      [name]: value
+    };
+    //console.log(`sending updated ${name} value to server: ${JSON.stringify(data)}`);
+    socket.emit(name, data);
+  }
 </script>
 
 <p>{name} {value}</p>
-<input type="range" bind:value {min} {max} {step} />
+<input type="range" bind:value {min} {max} {step} on:change={handleChange} />
