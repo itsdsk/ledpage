0f89f8dd1f01019e2370b635b5c2fd5f605ef9c8
itsdsk <dsuttonklein@mail.com>
2020-03-24 19:02:54 +0000

    public: simplify configuration component (remove led map) and restyle as css grid

--- public/svelte-components/src/Configuration.svelte
+++ public/svelte-components/src/Configuration.svelte
@@ -4,16 +4,6 @@
   import MapChain from "./MapChain.svelte";
   import { config } from './stores.js';
 
-  let activeChain;
-  let showAll = true;
-
-  $: brightness = {
-    name: "brightness",
-    value: $config.settings.brightness || 0.0125,
-    min: 0.0,
-    max: 1.0,
-    step: 0.0005
-  };
   $: desaturation = {
     name: "desaturation",
     value: $config.settings.desaturation || 0.0,
@@ -59,61 +49,28 @@
 
 <div id="config-main">
 
-  {#if showAll}
-
-    {#if $config}
-      <div id="config-main__map">
-      
-        <MapContainer>
-          {#each $config.outputs as output}
-            <MapChain fillColour={activeChain === output ? 'black' : 'white'} {output} on:click={() => { activeChain = output; }}/>
-          {/each}
-        </MapContainer>
-      
-        <p>Output has {$config.outputs.reduce((accumulator, currentValue) => {return accumulator + currentValue.leds.length}, 0)} LEDs in {$config.outputs.length} chains</p>
-      
-        <input type="file" accept="application/json" style="display:none" on:change={handleFiles}>
-        <a href="#" on:click|preventDefault|stopPropagation={() => document.querySelector("input[type='file']").click()}>Upload config file</a>
-        {#if fileUploadText.length}
-          <p>{fileUploadText}</p>
-        {/if}
-
-        <p>
-          Chain:
-          {#if activeChain}
-            #{activeChain.index}: Type: {activeChain.properties.type} LEDs: {activeChain.leds.length}
-          {:else}
-            None selected...
-          {/if}
-        </p>
-
-      </div>
-
+  <div>
+    <input type="file" accept="application/json" style="display:none" on:change={handleFiles}>
+    <a href="#" on:click|preventDefault|stopPropagation={() => document.querySelector("input[type='file']").click()}>Upload config file</a>
+    {#if fileUploadText.length}
+      <p>{fileUploadText}</p>
     {/if}
+  </div>
 
+  {#each [desaturation, gamma, blur, fade, autoplayDurationMin, autoplayDurationMax] as item}
     <div>
-      {#each [brightness, desaturation, gamma, blur, fade, autoplayDurationMin, autoplayDurationMax] as item}
-        <div>
-          <ConfigurationSlider {...item} />
-        </div>
-      {/each}
+      <ConfigurationSlider {...item} />
     </div>
-  {/if}
+  {/each}
+
 </div>
 
 <style>
   #config-main {
-    display: flex;
+    display: grid;
+    grid-template-columns: repeat(auto-fill,minmax(250px, 1fr));
+    grid-column-gap: 10px;
+    grid-row-gap: 10px;
     justify-content: center;
-    flex-wrap: wrap;
-    flex-direction: row;
-    align-items: flex-start;
-    background-color: gray;
-    margin: 8px;
-  }
-  #config-main__map {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
   }
 </style>
