b48cf733267eb0b855055fc2be786adb7a83169d
itsdsk <dsuttonklein@mail.com>
2020-04-09 21:36:31 +0100

    public: add save button to config sliders that appears if changed, and disable input by default until user clicks

--- public/svelte-components/src/ConfigurationSlider.svelte
+++ public/svelte-components/src/ConfigurationSlider.svelte
@@ -5,6 +5,9 @@
   export let max;
   export let step;
 
+  let readonly = true;
+  let changed = false;
+
   function handleChange (event) {
     var data = {
       name: name,
@@ -12,8 +15,27 @@
     };
     //console.log(`sending updated ${name} value to server: ${JSON.stringify(data)}`);
     socket.emit("config/update", data);
+    changed = true;
   }
 </script>
 
-<p>{name} {value}</p>
-<input type="range" bind:value {min} {max} {step} on:change={handleChange} />
+<label>
+  <span>{name.toUpperCase()}:</span>
+  <input type="number" bind:value {min} {max} {step} {readonly} on:click|once="{() => readonly = false}" on:change={handleChange} />
+{#if changed}
+  <button disabled={!changed} on:click={() => socket.emit('saveconfig')}>Save</button>
+{/if}
+</label>
+
+
+<style>
+  label, span {
+    display: block;
+    text-align: left;
+  }
+
+  span {
+    font-size: 1em;
+    font-weight: bold;
+  }
+</style>
