ca9ef98bf69ea4fba6fbf07a1ee7a603b45fd683
itsdsk <dsuttonklein@mail.com>
2020-04-16 21:10:52 +0100

    public: add buttons to quick change config values and restyle config input

--- public/svelte-components/src/ConfigurationSlider.svelte
+++ public/svelte-components/src/ConfigurationSlider.svelte
@@ -17,25 +17,64 @@
     socket.emit("config/update", data);
     changed = true;
   }
+
+  function handleBtnClick (event) {
+    // get value mapped from 0.0 to 1.0
+    var val = parseInt(event.target.innerHTML)/100;
+    // map from min to max
+    val = val * Math.abs(max - min);
+    val = min + val;
+    // switch to int
+    if (Number.isInteger(step)) {
+      val = Math.round(val);
+    }
+    // update client and server
+    value = val;
+    handleChange();
+  }
 </script>
 
-<label>
-  <span>{name.toUpperCase()}:</span>
-  <input type="number" bind:value {min} {max} {step} {readonly} on:click|once="{() => readonly = false}" on:change={handleChange} />
-{#if changed}
-  <button disabled={!changed} on:click={() => socket.emit('saveconfig')}>Save</button>
-{/if}
-</label>
-
+<div>
+  <label for={name}>
+    {name.toUpperCase()}:
+  </label>
+  <div class="config-input-save">
+    <input id={name} type="number" bind:value {min} {max} {step} {readonly} on:click|once="{() => readonly = false}" on:change={handleChange} />
+    <button disabled={readonly} on:click={handleBtnClick}>0</button>
+    <button disabled={readonly} on:click={handleBtnClick}>33</button>
+    <button disabled={readonly} on:click={handleBtnClick}>66</button>
+    <button disabled={readonly} on:click={handleBtnClick}>100</button>
+  </div>
+</div>
 
 <style>
-  label, span {
+  label {
     display: block;
     text-align: left;
-  }
-
-  span {
     font-size: 1em;
     font-weight: bold;
+  }
+
+  .config-input-save {
+    display: flex;
+  }
+
+  .config-input-save > *:first-child {
+    flex-grow: 2;
+  }
+
+  div {
+    padding-top: 1em;
+  }
+
+  button {
+    margin-left: 0.5em;
+    width: 3.5em;
+    padding: 0;
+    text-align: center;
+  }
+
+  input {
+    width: 4.5em;
   }
 </style>
