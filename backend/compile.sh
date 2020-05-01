892902af4b59f70df9aa22e63cd240b12b496d15
itsdsk <dsuttonklein@mail.com>
2020-05-01 23:56:57 +0100

    backend: build and install pigpio library in compilation script

--- backend/compile.sh
+++ backend/compile.sh
@@ -3,11 +3,17 @@
 # clone dependencies
 git submodule update
 
-# install GPIO library
+# install bcm2835 library
 cd ./device
 chmod +x ./install_libBCM2835.sh
 ./install_libBCM2835.sh
 cd ../
+
+# install pigpio library
+cd ./thirdparty/pigpio
+make
+sudo make install
+cd ../../
 
 # compile
 cmake ./
