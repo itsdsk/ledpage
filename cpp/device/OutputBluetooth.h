3c18e898c4946db79abe7132ce0c5822c0340485
Daniel Sutton-Klein <dsuttonklein@mail.com>
2020-09-09 15:45:28 +0100

    backend: make bluetooth output device re-attempt connection if disconnected during runtime / after object construction

--- cpp/device/OutputBluetooth.h
+++ cpp/device/OutputBluetooth.h
@@ -14,8 +14,7 @@ class OutputBluetooth : public Output
 public:
     OutputBluetooth(const json &properties)
     {
-        // check properties object contains serial port and baud rate values, and set defaults if not
-        std::string MAC_addr = "00:00:00:00:00:00";
+        // check properties object contains MAC address of bluetooth device
         if (properties.contains("MAC"))
         {
             MAC_addr = properties["MAC"];
@@ -23,7 +22,7 @@ public:
         }
         else
         {
-            std::cout << "no MAC address specified in properties, using default: " << MAC_addr << std::endl;
+            std::cout << "No MAC address for bluetooth device specified in properties" << std::endl;
         }
         struct sockaddr_rc addr = {0};
         int status;
@@ -59,6 +58,27 @@ public:
             _ledBuffer[4] = (ledValues.size() - 1) & 0xFF;        // LED count low byte
             _ledBuffer[5] = _ledBuffer[3] ^ _ledBuffer[4] ^ 0x55; // Checksum
         }
+        // check whether to reconnect to bluetooth
+        if (num_connection_errors % max_connection_errors == max_connection_errors - 1) {
+            std::cout << "Reconnecting to bluetooth: " << MAC_addr << std::endl;
+            // attempt to reconnect to bluetooth
+            struct sockaddr_rc addr = {0};
+            int status;
+            // allocate socket
+            bluetooth_sock = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+            // set the connection parameters (who to connect to)
+            addr.rc_family = AF_BLUETOOTH;
+            addr.rc_channel = (uint8_t)1;
+            str2ba(MAC_addr.c_str(), &addr.rc_bdaddr);
+            // connect to server
+            status = connect(bluetooth_sock, (struct sockaddr *)&addr, sizeof(addr));
+            if (status == 0) {
+                std::cout << "Success opening Bluetooth device" << std::endl;
+                connection_status = true;
+            } else {
+                perror("Error opening bluetooth");
+            }
+        }
         // copy led data to mesage
         memcpy(6 + (char*)_ledBuffer.data(), ledValues.data(), ledValues.size() * 3);
         // write data
@@ -68,12 +88,18 @@ public:
             if (status < 0)
             {
                 perror("Bluetooth TX Error");
+                // increment counter
+                num_connection_errors++;
             }
             else
             {
-                //std::cout << "sending out of Bluetooth\n" << std::endl; 
+                // bluetooth write successful, reset counter
+                num_connection_errors = 0;
             }
             
+        } else {
+            // no bluetooth connection, increment counter
+            num_connection_errors++;
         }
         return 0;
     }
@@ -87,4 +113,7 @@ public:
     std::string _ledBuffer = "";
     int bluetooth_sock = -1;
     bool connection_status = false;
+    std::string MAC_addr = "00:00:00:00:00:00";
+    unsigned num_connection_errors = 0;
+    unsigned max_connection_errors = 2000;
 };
