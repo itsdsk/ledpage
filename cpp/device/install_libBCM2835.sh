505a1653b3e6d74f8c94dffa4e70e8bdebf5c90c
Daniel Sutton-Klein <dsuttonklein@mail.com>
2021-03-28 18:38:04 +0200

    update to latest version of BCM2385 library

--- cpp/device/install_libBCM2835.sh
+++ cpp/device/install_libBCM2835.sh
@@ -1,8 +1,8 @@
 # install SPI library
 
-wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz
-tar zxvf bcm2835-1.60.tar.gz
-cd bcm2835-1.60/
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.68.tar.gz
+tar zxvf bcm2835-1.68.tar.gz
+cd bcm2835-1.68/
 ./configure
 make
 sudo make check
