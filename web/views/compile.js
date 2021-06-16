0ce9115f7099a6edbf98e675752db44b9cd41070
Daniel Sutton-Klein <dsuttonklein@mail.com>
2021-06-16 22:47:35 +0200

    ui: refactor svelte compiler script to detect page based on URL

--- web/views/compile.js
+++ web/views/compile.js
@@ -2,15 +2,14 @@ import PageMain from './PageMain.svelte';
 
 import Body from './Body.svelte';
 
-if (document.querySelector('#page-main') != null) {
+if (!window.location.pathname.includes('nxt') && document.querySelector('#page-main') != null) {
 	new PageMain({
 		target: document.querySelector('#page-main')
 	});
 }
 
-if (document.querySelector('#remove-me') != null) {
+if (window.location.pathname.includes('nxt')) {
 	new Body({
-		// change selector to 'body' element when old html is deleted
-		target: document.querySelector('#remove-me')
+		target: document.querySelector('body')
 	});
 }
