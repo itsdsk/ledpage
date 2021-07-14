b418df7cb72df3060e2e922cac5e3b9943cb7fa2
Daniel Sutton-Klein <dsuttonklein@mail.com>
2021-07-14 08:03:18 +0200

    webui: update HTML files, switch `index.html` to the new version

--- web/views/compile.js
+++ web/views/compile.js
@@ -4,20 +4,16 @@ import Body from './Body.svelte';
 
 import Setup from './Setup.svelte';
 
-if (!window.location.pathname.includes('nxt') && document.querySelector('#page-main') != null) {
+if (window.location.pathname.includes('setup')) {
+	new Setup({
+		target: document.querySelector('body')
+	});
+} else if (window.location.pathname.includes('old')) {
 	new PageMain({
 		target: document.querySelector('#page-main')
 	});
-}
-
-if (window.location.pathname.includes('nxt')) {
+} else {
 	new Body({
-		target: document.querySelector('body')
-	});
-}
-
-if (window.location.pathname.includes('setup')) {
-	new Setup({
 		target: document.querySelector('body')
 	});
 }
