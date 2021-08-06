f050c471da3e61b681541fc0d03638a76bab3595
Daniel Sutton-Klein <dsuttonklein@mail.com>
2021-08-06 14:39:14 +0200

    webui: add commands to compile `index` and `setup` pages separately from each other

--- web/rollup.config.js
+++ web/rollup.config.js
@@ -9,105 +9,122 @@ import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
-export default [{
-		input: 'views/compile_index.js',
-		output: {
-			sourcemap: true,
-			format: 'iife',
-			name: 'app',
-			file: '../public/index-bundle.js'
-		},
-		plugins: [
-			svelte({
-				compilerOptions: {
-					// enable run-time checks when not in production
-					dev: !production
-				}
-			}),
-
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css({
-				output: 'index-bundle.css'
-			}),
-
-			// If you have external dependencies installed from
-			// npm, you'll most likely need these plugins. In
-			// some cases you'll need additional configuration -
-			// consult the documentation for details:
-			// https://github.com/rollup/plugins/tree/master/packages/commonjs
-			resolve({
-				browser: true,
-				dedupe: ['svelte']
-			}),
-			commonjs(),
-
-			// In dev mode, call `npm run start` once
-			// the bundle has been generated
-			!production && serve(),
-
-			// Watch the `public` directory and refresh the
-			// browser on changes when not in production
-			!production && livereload('../public'),
-
-			// If we're building for production (npm run build
-			// instead of npm run dev), minify
-			production && terser()
-		],
-		watch: {
-			clearScreen: false
-		}
+let indexconfig = {
+	input: 'views/compile_index.js',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'app',
+		file: '../public/index-bundle.js'
 	},
-	{
-		input: 'views/compile_setup.js',
-		output: {
-			sourcemap: true,
-			format: 'iife',
-			name: 'app',
-			file: '../public/setup-bundle.js'
-		},
-		plugins: [
-			svelte({
-				compilerOptions: {
-					// enable run-time checks when not in production
-					dev: !production
-				}
-			}),
+	plugins: [
+		svelte({
+			onwarn: (warning, handler) => {
+				// don't warn on socketio because it is constructed in HTML
+				if (warning.message === "'socket' is not defined") return;
+				// let Rollup handle all other warnings normally
+				handler(warning);
+			},
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
+			}
+		}),
 
-			// we'll extract any component CSS out into
-			// a separate file - better for performance
-			css({
-				output: 'setup-bundle.css'
-			}),
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
+		css({
+			output: 'index-bundle.css'
+		}),
 
-			// If you have external dependencies installed from
-			// npm, you'll most likely need these plugins. In
-			// some cases you'll need additional configuration -
-			// consult the documentation for details:
-			// https://github.com/rollup/plugins/tree/master/packages/commonjs
-			resolve({
-				browser: true,
-				dedupe: ['svelte']
-			}),
-			commonjs(),
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		resolve({
+			browser: true,
+			dedupe: ['svelte']
+		}),
+		commonjs(),
 
-			// In dev mode, call `npm run start` once
-			// the bundle has been generated
-			!production && serve(),
+		// In dev mode, call `npm run start` once
+		// the bundle has been generated
+		!production && serve(),
 
-			// Watch the `public` directory and refresh the
-			// browser on changes when not in production
-			!production && livereload('../public'),
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
+		!production && livereload('../public'),
 
-			// If we're building for production (npm run build
-			// instead of npm run dev), minify
-			production && terser()
-		],
-		watch: {
-			clearScreen: false
-		}
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser()
+	],
+	watch: {
+		clearScreen: false
 	}
-];
+};
+
+let setupconfig = {
+	input: 'views/compile_setup.js',
+	output: {
+		sourcemap: true,
+		format: 'iife',
+		name: 'app',
+		file: '../public/setup-bundle.js'
+	},
+	plugins: [
+		svelte({
+			onwarn: (warning, handler) => {
+				// don't warn on socketio because it is constructed in HTML
+				if (warning.message === "'socket' is not defined") return;
+				// let Rollup handle all other warnings normally
+				handler(warning);
+			},
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
+			}
+		}),
+
+		// we'll extract any component CSS out into
+		// a separate file - better for performance
+		css({
+			output: 'setup-bundle.css'
+		}),
+
+		// If you have external dependencies installed from
+		// npm, you'll most likely need these plugins. In
+		// some cases you'll need additional configuration -
+		// consult the documentation for details:
+		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+		resolve({
+			browser: true,
+			dedupe: ['svelte']
+		}),
+		commonjs(),
+
+		// In dev mode, call `npm run start` once
+		// the bundle has been generated
+		!production && serve(),
+
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
+		!production && livereload('../public'),
+
+		// If we're building for production (npm run build
+		// instead of npm run dev), minify
+		production && terser()
+	],
+	watch: {
+		clearScreen: false
+	}
+};
+
+var to_export = process.env.INDEX ? indexconfig :
+	process.env.SETUP ? setupconfig : [indexconfig, setupconfig];
+
+export default to_export;
 
 function serve() {
 	let started = false;
